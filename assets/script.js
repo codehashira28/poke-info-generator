e68f2c82a7fb885effc173957ae5737e07faf9c1
James Belcher <130000791+Tankb220@users.noreply.github.com>
2023-05-30 19:39:13 -0500

    turned pokemon logo into button and directions on how to use page

--- assets/script.js
+++ assets/script.js
@@ -11,7 +11,7 @@ for(let i = 1; i <= 9; ++i) {
 // function to generate list of pokemon based on generation selected
 
 function generateList(event) {
-    heading.textContent = "Select a pokemon to see its data";
+    heading.textContent = "Select a pokemon";
     pokemonlist.innerHTML = "";
     var mypokemon = [];
     fetch("https://pokeapi.co/api/v2/generation/" + event.target.id[event.target.id.length-1])
